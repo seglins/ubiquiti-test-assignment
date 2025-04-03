6e9f86e82c8709e0b44261a6e103bf6f339d2e74
Bruno <seglinbruno@gmail.com>
2025-04-03 14:19:10 +0300

    Add UI components:
    - Button
    - Checkbox
    - Icon
    - Logo
    - Table

---
+++ lib/components/ui/table.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import cn from "classnames";
+import { useRouter } from "next/navigation";
+import { type CSSProperties } from "react";
+
+interface TableColumn {
+  title: string;
+  width?: CSSProperties["width"];
+}
+
+interface TableRow {
+  data: React.ReactNode[];
+  href?: string;
+}
+
+interface TableProps {
+  columns: TableColumn[];
+  rows: TableRow[];
+  className?: string;
+}
+
+const Table = ({ columns, rows, className }: TableProps) => {
+  const router = useRouter();
+
+  return (
+    <div className={cn("w-full relative overflow-x-auto", className)}>
+      <table className="w-full border-separate border-spacing-0">
+        <thead className="sticky top-0 bg-white/85 backdrop-blur-xs">
+          <tr className="">
+            {columns.map(({ title, width }, index) => (
+              <th
+                style={{ width, minWidth: width }}
+                key={index}
+                className={cn(
+                  "px-2 py-1.5 text-text-1 border-b border-neutral-3 text-left whitespace-nowrap",
+                  { "box-content": !!width },
+                )}
+              >
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row, rowIndex) => {
+            const { href } = row;
+
+            return (
+              <tr
+                key={rowIndex}
+                tabIndex={0}
+                onClick={() => {
+                  if (href) {
+                    router.push(href);
+                  }
+                }}
+                className={cn(
+                  "group hover:bg-neutral-3 focus-visible:outline-none focus:outline-none",
+                  { "cursor-pointer": !!href },
+                )}
+              >
+                {row.data?.map((cell, cellIndex) => (
+                  <td
+                    key={cellIndex}
+                    className={cn(
+                      "relative px-2 py-1.5 border-b border-neutral-3 border-t border-t-transparent text-left whitespace-nowrap",
+                      "group-focus:border-outline group-focus-visible:border-outline",
+                    )}
+                  >
+                    {cell}
+                  </td>
+                ))}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
