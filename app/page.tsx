ef8e524b86fc686db2c31825019ee3cccf3fac14
Bruno <seglinbruno@gmail.com>
2025-04-07 00:39:37 +0300

    Filter devices by name on server

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 import DeviceList from "@/lib/components/device/list";
+import { filterDevicesByName } from "@/lib/helpers";
 import { getDevices } from "@/lib/services/device";
 
-export default async function Home() {
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}) {
+  const { search } = await searchParams;
   const devices = await getDevices();
 
-  return <DeviceList devices={devices} />;
+  return (
+    <DeviceList
+      devices={
+        search && typeof search === "string"
+          ? filterDevicesByName(devices, search)
+          : devices
+      }
+    />
+  );
 }
